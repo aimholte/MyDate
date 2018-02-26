e218f11f72890d2e3289b77733d1965482353ec7
joshuabartz <jbartz@macalester.edu>
2018-02-26 16:38:10 -0600

    Thingy

---
+++ src/JsonJavaTest.js
@@ -0,0 +1,11 @@
+
+class placeplace{
+    constructor( name, types){
+        this.name=name;
+        this.types =types;
+    }
+
+    alerter(){
+        alert("Name: " + this.name + "\n" + "Types: "+ this.types);
+    }
+}
