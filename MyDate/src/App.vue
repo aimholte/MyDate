da0c35de0f116961af94817b6b3498c4a040af9e
kiantemiles <kmiles@macalester.edu>
2018-04-25 07:24:53 -0500

    About page and button fixed

--- MyDate/src/App.vue
+++ MyDate/src/App.vue
@@ -46,11 +46,51 @@ import header from './components/Header'
     margin-right: 0px;
 
 
+
     /*background-color: #323b39;*/
 
 
     /*background-image: url("./assets/PerfectDAte.png");*/
+  }
+  html,body{
+    min-height: 100vh;
+    padding:0;
+    margin:0;
+  }
+  nav{
+    background: #fd5e53 ;
+    padding: 14px 0;
+    margin-bottom: 0;}
 
+  img{
+    width: 75px;
+    height: 75px;
+  }
+  .column{
+    float: left;
+    padding: 10px;
+    column-gap: 10px;
+  }
+  .left, .right {
+    width: 450px;
+  }
+  .middle{
+    width: 450px;
+  }
+  .row{
+    content: "";
+    display: table;
+    clear: both;
+  }
+  button{
+    padding-left: 25px;
+    padding-right: 25px;
+    border-radius: 30px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    color:#323b39 ;
+    font-size: 18px;
+    background:  #fd5e53;
 
   }
 </style>
